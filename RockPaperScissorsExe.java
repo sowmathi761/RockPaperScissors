3132c9ef598f51e41e401c6d3a5b1028c32612ee
sowmathi761 <sowmathi37@gmail.com>
2025-11-22 18:06:53 +0530

    Implement Rock-Paper-Scissors game

---
+++ RockPaperScissorsExe.java
@@ -0,0 +1,140 @@
+import java.util.*;
+
+class Game {
+    private Scanner scn;
+    private String[] availableMoves = {"Rock", "Paper", "Scissors"};
+    private Random random;
+
+    public Game() {
+        scn = new Scanner(System.in);
+        random = new Random();
+    }
+
+    // Method to get computer move
+    private String getComputerMove() {
+        return availableMoves[random.nextInt(availableMoves.length)];
+    }
+
+    // Method to get user move with exception handling
+    private String getUserMove() {
+        String userMove = "";
+        while (true) {
+            try {
+                System.out.println("Please choose your move from these available moves : 'Rock' 'Paper' 'Scissors' ");
+                System.out.println("Enter the move you chose : ");
+                userMove = scn.nextLine();
+
+                if (userMove.equals("Rock") || userMove.equals("Paper") || userMove.equals("Scissors")) {
+                    System.out.println();
+                    break;
+                } else {
+                    throw new IllegalArgumentException("Invalid Move!! Please enter only Rock, Paper, or Scissors.");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println();
+                System.out.println(e.getMessage());
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println("Unexpected error occurred: " + e.getMessage());
+                System.out.println("Please try again.");
+            }
+        }
+        return userMove;
+    }
+
+    // Method to decide winner
+    private void decideWinner(String userMove, String computerMove) {
+        System.out.println("Computer chose : " + computerMove);
+
+        if (userMove.equals(computerMove)) {
+            System.out.println("Its a tie!");
+        } else if (userMove.equals("Rock")) {
+            if (computerMove.equals("Paper")) {
+                System.out.println("Computer won!");
+                System.out.println("Better luck next time!");
+            } else {
+                System.out.println("You won!");
+                System.out.println("Congratulations!");
+            }
+        } else if (userMove.equals("Paper")) {
+            if (computerMove.equals("Rock")) {
+                System.out.println("You won!");
+                System.out.println("Congratulations!");
+            } else {
+                System.out.println("Computer won!");
+                System.out.println("Better luck next time!");
+            }
+        } else if (userMove.equals("Scissors")) {
+            if (computerMove.equals("Paper")) {
+                System.out.println("You won!");
+                System.out.println("Congratulations!");
+            } else {
+                System.out.println("Computer won!");
+                System.out.println("Better luck next time!");
+            }
+        }
+    }
+
+    // Method to ask play again with exception handling
+    private boolean playAgain() {
+        String playAgain = "";
+        System.out.println();
+        System.out.println("Do you want to play again? ");
+        while (true) {
+            try {
+                System.out.println("Type 'yes' or 'no' ");
+                playAgain = scn.nextLine();
+
+                if (playAgain.equalsIgnoreCase("yes") || playAgain.equalsIgnoreCase("no")) {
+                    System.out.println();
+                    System.out.println("*****************************************************************************");
+                    System.out.println();
+                    break;
+                } else {
+                    throw new IllegalArgumentException("Invalid Input! Please type only 'yes' or 'no'.");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println("Unexpected error occurred: " + e.getMessage());
+                System.out.println("Please try again.");
+            }
+        }
+        return playAgain.equalsIgnoreCase("yes");
+    }
+
+    // Method to start the game
+    public void start() {
+        while (true) {
+            try {
+                String computerMove = getComputerMove();
+                System.out.println("Computer has chosen its move.");
+                System.out.println();
+                System.out.println("Now it's your turn to choose. Good Luck!");
+                System.out.println();
+
+                String userMove = getUserMove();
+                decideWinner(userMove, computerMove);
+
+                if (!playAgain()) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Something went wrong during the game: " + e.getMessage());
+                System.out.println("Restarting the round...");
+            }
+        }
+    }
+}
+
+public class RockPaperScissor {
+    public static void main(String[] args) {
+        try {
+            Game game = new Game();
+            game.start();
+        } catch (Exception e) {
+            System.out.println("Critical error: " + e.getMessage());
+        }
+    }
+}
